d6cb9c27267d1836edc44edfac356bd3aa8686f4
Kieran Groble <grobleke@GROBLEKE-W540.rose-hulman.edu>
2017-01-20 11:17:09 -0500

    less unique unique IDs

--- src/graphviz/GraphvizEdge.java
+++ src/graphviz/GraphvizEdge.java
@@ -15,14 +15,14 @@ public class GraphvizEdge extends GraphvizElement {
     public String toGraphviz() {
         String code = "";
         code += '\"' + this.fromNode + "\" -> \"" + this.toNode + "\"[\n\t";
-        Object[] attributeArray = this.attributes.keySet().toArray();
+        String[] attributeArray = this.attributes.keySet().toArray(new String[0]);
         for(int i = 0; i < attributeArray.length - 1; i++) {
-            code += (String) attributeArray[i] + " = " + this.attributes.get((String) attributeArray[i]);
+            code += attributeArray[i] + " = " + this.attributes.get(attributeArray[i]);
             code += ",\n\t";
         }
         if (attributeArray.length > 0) {
             code += attributeArray[attributeArray.length - 1]
-                    + " = " + this.attributes.get((String) attributeArray[attributeArray.length - 1])
+                    + " = " + this.attributes.get(attributeArray[attributeArray.length - 1])
                     + "\n];\n";
         } else {
             code += "\n]\n";
@@ -33,7 +33,8 @@ public class GraphvizEdge extends GraphvizElement {
 
     @Override
     public String getIdentifier() {
-        return this.fromNode + "-" + this.toNode + "<" + type + ">";
+        String names = fromNode.compareTo(toNode) < 0 ? fromNode + "-" + toNode : toNode + fromNode;
+        return names + "<" + type + ">";
     }
 
 }
